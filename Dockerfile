6acdb52b699e3e7a9969772ed962c2426ed5cb38
Andrey Sizov <andrey.sizov@jetbrains.com>
2015-07-27 17:06:32 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM sergeyzh/centos6-java
+MAINTAINER Andrey Sizov, andrey.sizov@jetbrains.com
+
+ENV LFS_VERSION 4.2.0
+
+ADD http://www.moonlit-software.com/logfaces/downloads/lfs.${LFS_VERSION}.linux.x86-64.tar.gz /root/
+RUN tar zxf /root/lfs.${LFS_VERSION}.linux.x86-64.tar.gz -C /root/
+
+RUN sed -i '1 a JAVA_HOME=/usr/java64/current' /root/logFacesServer/bin/lfs
+RUN sed -i '2 a APP_BIN=/root/logFacesServer/bin' /root/logFacesServer/bin/lfs
+RUN sed -i 's/WRAPPER_CMD=\"\./WRAPPER_CMD=\"\$\{APP_BIN\}/' /root/logFacesServer/bin/lfs
+RUN sed -i 's/WRAPPER_CONF=\"\./WRAPPER_CONF=\"\$\{APP_BIN\}/' /root/logFacesServer/bin/lfs
+
+ENV JAVA_HOME /usr/java64/current
+ENV PATH $PATH:$JAVA_HOME/bin
+
+ADD run-services.sh /
+RUN chmod +x /run-services.sh
+CMD /run-services.sh
+
+EXPOSE 8050 55200 1468 55201 514
---
+++ run-services.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+rccheck() {
+   if [[ $? != 0 ]]; then
+      echo "Error! Stopping the script."
+      exit 1
+   fi
+}
+
+if [ ! -f /root/logFacesServer/conf/lfs.xml ];
+then
+   echo "Looks like it is the first run of lfs service"
+   echo -n "Generating configs... " ; /root/logFacesServer/bin/lfs start > /dev/null 2>&1 ; rccheck
+   while [ ! -f /root/logFacesServer/conf/lfs.xml ];
+   do
+      sleep 2
+   done
+   /root/logFacesServer/bin/lfs stop > /dev/null 2>&1 ; rccheck ; echo "done!"
+fi
+
+if [ -f /conf/lfs.xml ]; then
+   echo -n "Setting our configs instead of typical... "
+
+   rm -rf /root/logFacesServer/db/ ; rccheck
+   if [ ! -f /root/logFacesServer/conf/hosts.properties ]; then
+      touch /root/logFacesServer/conf/hosts.properties ; rccheck
+   fi
+   for i in `find /conf/ -type f`; do ln -fs $i /root/logFacesServer/conf/ ; done
+
+
+   echo "done!"
+fi
+if [ ${MONGO_URL} ]; 
+then
+   sed -i 's/com.moonlit.logfaces.config.mongodb=.*/com.moonlit.logfaces.config.mongodb=true/' /root/logFacesServer/conf/environment.properties
+   sed -i "s/com.moonlit.logfaces.config.mongodb.connection =.*/com.moonlit.logfaces.config.mongodb.connection=${MONGO_URL}/" /root/logFacesServer/conf/mongodb.properties
+
+   sed -i 's/^com.moonlit.logfaces.config.mongodb.dbname  =.*/com.moonlit.logfaces.config.mongodb.dbname  = lfsp/' /root/logFacesServer/conf/mongodb.properties
+   sed -i 's/^com.moonlit.logfaces.config.mongodb.ttlDays =.*/com.moonlit.logfaces.config.mongodb.ttlDays = 0/' /root/logFacesServer/conf/mongodb.properties
+   sed -i 's/^com.moonlit.logfaces.config.mongodb.partitioned =.*/com.moonlit.logfaces.config.mongodb.partitioned = true/' /root/logFacesServer/conf/mongodb.properties
+   sed -i 's/^com.moonlit.logfaces.config.mongodb.pdays =.*/com.moonlit.logfaces.config.mongodb.pdays = 1/' /root/logFacesServer/conf/mongodb.properties
+
+fi
+
+trap "/root/logFacesServer/bin/lfs stop" SIGINT SIGTERM SIGHUP
+/root/logFacesServer/bin/lfs console &
+wait
